41e84b38d266f9b89b594d33a9baca3c3eac0392
1mkluzz <velrunebitz@gmail.com>
2021-01-25 23:46:25 +0000

    Login e Registo - Endireitar Metodos

--- Login.xaml.cs
+++ Login.xaml.cs
@@ -18,15 +18,14 @@ namespace QuizAppWPF
     public partial class Login : Page
     {
         public static string username { get; set; }
+        private IFirebaseClient client;
+        private FirebaseResponse res;
 
         public Login()
         {
-            //await LoginBtn_Click_1(object sender, RoutedEventArgs e);
             InitializeComponent();
         }
-
-        private IFirebaseClient client;
-            
+     
         private async void LoginBtnClick(object sender, RoutedEventArgs e)
         {
             string objname = ((Button)sender).Name;
@@ -52,7 +51,7 @@ namespace QuizAppWPF
             this.NavigationService.Navigate(registo);
         }
 
-        private async Task login()
+        private async Task<Task> login()
         {
 
             try
@@ -68,18 +67,25 @@ namespace QuizAppWPF
                string.IsNullOrWhiteSpace(passTbox.Password))
             {
                 MessageBox.Show("Preencha todos os campos!");
-                return;
             }
             #endregion
 
-            FirebaseResponse res = client.Get(@"Utilizadores/" + UsernameTbox.Text);
-            Utilizador ResUser = res.ResultAs<Utilizador>();    // Resultado da base de dados
+            res = client.Get(@"Utilizadores/" + UsernameTbox.Text);
+            
+            await VerifyLogin();
 
+            return Task.CompletedTask;
+
+        }
+
+        private Task VerifyLogin()
+        {
             Utilizador CurUser = new Utilizador()               // Informação do utilizador
             {
                 nomeUtilizador = UsernameTbox.Text,
                 Password = passTbox.Password
             };
+            Utilizador ResUser = res.ResultAs<Utilizador>();    // Resultado da base de dados
 
             //Caso os dados estejam corretos é redireccionado para a APP
             if (Utilizador.IsEqual(ResUser, CurUser))
@@ -93,9 +99,9 @@ namespace QuizAppWPF
             {
                 Utilizador.ShowError();
             }
-
+            return Task.CompletedTask;
         }
-    
+
     }
 
 }
